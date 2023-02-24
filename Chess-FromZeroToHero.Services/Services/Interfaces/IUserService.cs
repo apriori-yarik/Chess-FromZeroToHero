945d299cad03f6f911bdc84bf4bd40a09b0aa107
Yaroslav Dokuzov <yaroslav.dokuzov@primeholding.com>
2023-02-24 23:12:34 +0200

    User service done

---
+++ Chess-FromZeroToHero.Services/Services/Interfaces/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using Chess_FromZeroToHero.Contracts.Dtos.Pagination;
+using Chess_FromZeroToHero.Contracts.Dtos.User;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_FromZeroToHero.Services.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<UserWithIdDto> GetUserByIdAsync(Guid id);
+
+        Task<ICollection<UserWithIdDto>> GetUsersAsync(PaginationParams paginationParams);
+
+        Task CreateUserAsync(UserDto dto);
+
+        Task UpdateUserAsync(UserWithIdDto dto);
+
+        Task DeleteUserAsync(Guid id);
+    }
+}
---
+++ Chess-FromZeroToHero.Services/Services/UserService.cs
@@ -0,0 +1,33 @@
+﻿using Chess_FromZeroToHero.Contracts.Dtos.Pagination;
+using Chess_FromZeroToHero.Contracts.Dtos.User;
+using Chess_FromZeroToHero.DataAccess.Repositories.Interfaces;
+using Chess_FromZeroToHero.Services.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chess_FromZeroToHero.Services.Services
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task CreateUserAsync(UserDto dto) => await _userRepository.CreateUserAsync(dto);
+
+        public async Task DeleteUserAsync(Guid id) => await _userRepository.DeleteUserAsync(id);
+
+        public async Task<UserWithIdDto> GetUserByIdAsync(Guid id) => await _userRepository.GetUserByIdAsync(id);
+
+        public async Task<ICollection<UserWithIdDto>> GetUsersAsync(PaginationParams paginationParams) => await _userRepository.GetUsersAsync(paginationParams);
+
+        public async Task UpdateUserAsync(UserWithIdDto dto) => await _userRepository.UpdateUserAsync(dto);
+    }
+}
